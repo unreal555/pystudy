ab264d7e0abbf1c7f282eb3b98fb5e0f0b077eda
Administrator <47540479@qq.com>
2020-02-27 22:45:44 +0800

    Default

--- scrapy-demo/demo/demo/spiders/sunyunzhu.py
+++ scrapy-demo/demo/demo/spiders/sunyunzhu.py
@@ -28,26 +28,41 @@ class Sunyunzhu_Spider(scrapy.Spider):
         sign = hashlib.md5(sign.encode(encoding='utf-8')).hexdigest()
         req = 'http://api.fanyi.baidu.com/api/trans/vip/translate?q={}&from={}&to={}&appid={}&salt={}&sign={}'.format(q,f,t,APP_ID,salt,sign)
         print(req)
-        response = requests.get(req)
-        s = response.content.decode('unicode_escape')
-        s = json.loads(s)
-        s = s['trans_result'][0]['dst']
-        print(s, type(s))
-        print('输入{}翻译成{}'.format(q, s))
-        return s
+
+        def trans(q):
+            sign = APP_ID + q + salt + key
+            sign = hashlib.md5(sign.encode(encoding='utf-8')).hexdigest()
+            while 1:
+                req = 'http://api.fanyi.baidu.com/api/trans/vip/translate?q={}&from={}&to={}&appid={}&salt={}&sign={}'.format(
+                    q, f, t, APP_ID, salt, sign)
+                print(req)
+                response = requests.get(req)
+                s = response.content.decode('unicode_escape')
+                print(s)
+                if 'error_code' in s:
+                    time.sleep(1.5)
+                    continue
+                s = json.loads(s)
+                s = s['trans_result'][0]['dst']
+                print(s, type(s))
+                print('输入{}翻译成{}'.format(q, s))
+                return s
 
     def start_requests(self):
-        for i in range(1999474,1550000,-1):
+        for i in range(1999474,1000000,-1):
             yield scrapy.Request('https://www.t-nani.co.kr/shop/shopdetail.html?branduid={}&'.format(i),dont_filter=True,callback=self.parse)
+
 
     def parse(self,response):
         if len(re.findall('alert',response.text))!=1:
-            with open(os.path.join(IMAGES_STORE,sun_log.txt),'a',encoding='utf-8') as f:
+            with open(os.path.join(IMAGES_STORE,'sun_log.txt'),'a',encoding='utf-8') as f:
                 f.write(response.url+'\r\n')
             urls=[]
             for url in re.findall('src="(http://.*?\.jpg)">',response.text):
                 if 'jpg' not in url and  'jepg' not in url and  'png' not in url and  'bmp' not in url and  'gif' not in url:
-                    pass
+                    continue
+                if  'tnanilogo.jpg' in url or  'modelsize.jpg' in url :
+                    continue
 
                 if '?'in url:
                     urls.append(url.split('?')[0])
@@ -55,10 +70,6 @@ class Sunyunzhu_Spider(scrapy.Spider):
                 if 'http://' in url or 'https://' in url:
                     urls.append(url)
             urls=set(urls)
-
-
-
-
             print(urls)
             title=re.findall('<title>(.*?)</title>',response.text)[0]
             num_title=re.findall('branduid=(\d+)&',response.url)[0]
