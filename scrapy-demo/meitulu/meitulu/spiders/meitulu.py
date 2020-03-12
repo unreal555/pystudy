31b6830c08ad76d2a31f63805d97505401a1a7a4
unreal555 <47540479@qq.com>
2020-03-12 17:35:59 +0800

    1

--- scrapy-demo/meitulu/meitulu/spiders/meitulu.py
+++ scrapy-demo/meitulu/meitulu/spiders/meitulu.py
@@ -4,7 +4,7 @@ import re
 import os
 import sys
 import time
-sys.path.append(r'e:\\pycharm-professional-2017.2.4\\pystudy\\scrapy-demo\\meitulu\meitulu')
+sys.path.append(r'd:\\pycharm-professional-2017.2.4\\pystudy\\scrapy-demo\\meitulu\meitulu')
 from  items import PicItem
 from settings import IMAGES_STORE
 print(sys.path)
@@ -41,15 +41,15 @@ class MeiTuLu_Spider(scrapy.Spider):
 
         print('获得{}响应，开始处理'.format(response.url))
         print(response.url,response.status)
-        print(response.text)
 
+        print(response.text)
         if response.status!=200:
             print('页面不存在,返回')
             return
 
         page = re.sub('[\s]+', '', response.text)
-        select=re.findall('''<span><ahref="(/.*?/\d+.html)"target="_blank">(.*?)</a></span>''',page)
-
+        print(page)
+        select=re.findall('''<pclass=p_title><ahref="(.*?)"target="_blank">(.*?)</a></p>''',page)
 
         if flag==1:print(response.text)
 
@@ -58,63 +58,82 @@ class MeiTuLu_Spider(scrapy.Spider):
 
         with open(os.path.join(self.path, self.log_name), 'a', encoding='utf-8') as f:
             for i in select:
-                if i[0] not in log:
+                # if i[0] not in log:
+                #
+                #     print("写入log，新增相册{}".format(i[0],i[1]))
+                #     f.write('{}'.format(time.strftime( '%Y-%m-%d %H-%M') + '\t' +str(i[0])+'\t'+ str(i[1]) + '\r\n\r\n'))
+                #     print('提交新增加相册{}'.format(i[0],i[1]))
 
-                    print("写入log，新增相册{}".format(i[0],i[1]))
-                    f.write('{}'.format(time.strftime( '%Y-%m-%d %H-%M') + '\t' +str(i[0])+'\t'+ str(i[1]) + '\r\n\r\n'))
-                    print('提交新增加相册{}'.format(i[0],i[1]))
-                    print(response.url.split('list')[0]+i[0])
-                    yield scrapy.Request('https://www.aitaotu.com'+i[0], callback=self.get_page, dont_filter=True)
+                    yield scrapy.Request(i[0], callback=self.get_page, dont_filter=True)
 
-        # yield scrapy.Request(response.url, callback=self.get_page, dont_filter=True)
+
 
 
     def get_page(self,response):
         # page = re.sub('[\s+\.\!\/_,$%^*(+\"\')]+|[+——()?【】“”！，。？、~@#￥%……&*（）]+','',response.text )
 
         page = re.sub('[\s]+','',response.text )
-
-        select=re.findall('''当前位置：</span><ahref=".*?">(.*?)</a>.*?<ahref=".*?">(.*?)</a>&nbsp;&gt;&nbsp;(.*?)</div><divclass=".*?"id="photos">.*?<spanid="picnum">\(<spanclass="nowpage">.*?</span>/<spanclass="totalpage">(.*?)</span>\)''',page)[0]#搜索相册中所有的图片页面编号
-
-        print(select)
-
-        a,b,c,num=select
+        print(page)
+        select=re.findall('''<divclass="weizhi"><span>当前位置：<ahref=".*?">(.*?)</a>><ahref=".*?">(.*?)</a>></span><h1>(.*?)</h1></div>''',page)[0]#搜索相册中所有的图片页面编号
+        a,b,c=select
         a=re.sub('[\/:*?"<>|]','-',a)
         b = re.sub('[\/:*?"<>|]', '-', b)
         c = re.sub('[\/:*?"<>|]', '-', c)
-
+        if  '[' in c and ']' in c:
+            c=os.path.join(*re.split('[\[\]]',c))
         if flag==1:print(a,b,c)
-
         subdir=os.path.join(a,b,c)
         xiangce_path=os.path.join(self.path,subdir)
+
+        print(xiangce_path)
         if os.path.exists(xiangce_path):
             return
         else:
             os.makedirs(xiangce_path)
+        # des=[]
+        # print(re.findall('''<p>(发行机构：)<ahref=".*?"target="_blank"class="tags">(.*?)</a></p>''', page))
+
+        # print(re.findall('''<p>(模特姓名：)<ahref=".*?"target="_blank"class="tags">(.*?)</a></p>''', page))
+        # print(re.findall('''<p>(发行时间：.*?)</p></div>''', page)[0])
+        # print(re.findall('''<pclass="buchongshuoming"><span>(补充说明：)</span>(.*?)</p>''', page))
+
+
+        bianhao=re.findall('/(\d+)\.',response.url)[0]
+        max=re.findall('''<p>期刊编号：.*?</p><p>图片数量：(.*?)张</p>''', page)[0]
 
 
 
+
+
+        print(max)
         urls=[]
-        for i in range(1,int(num)+1,1):
-            urls.append('{}_{}.html'.format(response.url.split('.html')[0],i))
+        for i in range(1,int(max)+1):
+            urls.append('https://mtl.gzhuibei.com/images/img/{}/{}.jpg'.format(bianhao,i))
         print(urls)
         if flag==1:print(urls)
 
 
-
-        for url in urls:
-            yield scrapy.Request(url,  callback=self.get_pic_url, dont_filter=True,meta={'path':xiangce_path})
-
-
-    def get_pic_url(self,response):
-
-        urls=re.findall('''<a href=".*?"><img src="(.*?)" alt=".*?".*?></a>''',response.text)
-        urls.remove('https://img.aitaotu.cc:8089/Thumb/2018/footer_tp8.jpg')
-        xiangce_path=response.meta['path']
-
-        print('pic url =',urls)
-
         item=PicItem()
         item['image_urls']=urls
         item['image_path']=xiangce_path
+
         yield item
+
+
+#
+# #https://mtl.gzhuibei.com/images/img/20666/90.jpg,                    https://www.meitulu.com/item/20666.html>
+
+        # for url in urls:
+        #     yield scrapy.Request(url,  callback=self.get_pic_url, dont_filter=True,meta={'path':xiangce_path})
+
+
+    # def get_pic_url(self,response):
+    #
+    #     urls=re.findall('''<a href=".*?"><img src="(.*?)" alt=".*?".*?></a>''',response.text)
+    #     urls.remove('https://img.aitaotu.cc:8089/Thumb/2018/footer_tp8.jpg')
+    #     xiangce_path=response.meta['path']
+    #
+    #     print('pic url =',urls)
+    #
+    #
+    #
