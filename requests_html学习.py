0f1ff3cb5d5f38923f1ded278c711445583c5291
unreal555 <47540479@qq.com>
2020-03-27 21:27:19 +0800

    1

--- requests_html学习.py
+++ requests_html学习.py
@@ -13,7 +13,7 @@
 import asyncio
 import os
 import re
-os.environ['PYPPETEER_HOME'] = 'E:\pycharm-professional-2017.2.4'
+# os.environ['PYPPETEER_HOME'] = 'E:\pycharm-professional-2017.2.4'
 import pyppeteer.chromium_downloader
 print('默认版本是：{}'.format(pyppeteer.__chromium_revision__))
 print('可执行文件默认路径：{}'.format(pyppeteer.chromium_downloader.chromiumExecutable.get('win64')))
@@ -21,13 +21,53 @@ print('win64平台下载链接为：{}'.format(pyppeteer.chromium_downloader.dow
 import time,random
 from pyppeteer.launcher import launch # 控制模拟浏览器用
 from retrying import retry #设置重试次数用的
-from mytools import qu_kong_ge
+from mytools import qu_kong_ge,random_wait
 
-
-async def get_content(page,url):
-
+@retry
+async def goto(page,url):
     await page.goto(url)
-    time.sleep(3)
+
+async def get_chapter_content(page,chapter_start_url):
+
+    await goto(page,chapter_start_url)
+
+    content=await page.content()
+
+    chapter_name=re.findall('<h1 class="page-title">(.*?)</h1>',content)[0]
+    print(chapter_name)
+
+    chapter_page_list=[chapter_start_url]
+    for i in re.findall('<a href="(\d+_\d+\.html)">【.*?】</a>',content,re.S):
+        chapter_page_list.append(url+i)
+    print(chapter_page_list)
+
+    next_chapter_url=qu_kong_ge(re.findall('''<a href=".*?" class="prev"><span>&lt;</span>上一章</a>.*?<a href="(.*?)" class="next">下一章<span>&gt;</span></a>''',content,re.S)[0])
+    print(next_chapter_url)
+
+
+    content=''
+    for i in chapter_page_list:
+        content=content+get_text(await get_chapter_page_content(page,i))
+
+    content = content.replace('”', '」')
+    content = content.replace('“', '「')
+    content = content.replace(' ', '').replace('\r', '').replace('\n', '')
+    content = content.replace('。」', '」。')
+    content = content.replace('。', '。~!@#$')
+    content = content.replace('」。', '。」')
+    content = content.split('~!@#$')
+
+    for i in content:
+        print(i)
+    print(next_chapter_url)
+
+    return content,next_chapter_url
+
+async def get_chapter_page_content(page,url):
+
+    await goto(page,url)
+
+    random_wait(1,3)
     await page.evaluate('window.scrollBy(0, document.body.scrollHeight)')
 
     try:
@@ -35,9 +75,9 @@ async def get_content(page,url):
         s=await page.evaluate('''() =>  document.querySelector("#content").innerText''')
     except Exception as e:
         print(e)
+    return s
 
-    return await get_text(s)
-async def get_text(s):
+def get_text(s):
 
     result=''
     for i in re.split('\r|\n|\r\n',s):
@@ -47,26 +87,34 @@ async def get_text(s):
             # print(e)
             continue
         if '   ' in  i:
-            print(i)
+            # print(i)
             result=result+qu_kong_ge(i)
     return result
 
 async def main(url):# 定义main协程函数，
-    browser = await launch({'headless': False ,'args':['--window-size=16,12']})#,'--no-sandbox'
+    browser = await launch({'headless':False ,'args':['--window-size=16,12']})#,'--no-sandbox'
     page = await browser.newPage()
     await page.setUserAgent( 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36')
-    await page.goto(url)
-    print('开始阅读')
-    await page.evaluate('''document.querySelector("body > div.container > div.mod.detail > div.ft > table > tbody > tr > td:nth-child(1) > a").click()''')
-    print('1111')
-    num=re.findall('【(.*?)】',await page.evaluate('''() =>  document.querySelector("#content").innerText'''))
-    print(num)
+    await goto(page,url)
+    novel_name=re.findall('<h1>(.*?)</h1>',await page.content())[0]
+    print(novel_name)
+
+
+    chapter_start_url=url+re.findall('<a class="read start" href="(.*?)">.*?</a>',await page.content())[0]
+    while 'html' in chapter_start_url:
+        print(chapter_start_url,url,chapter_start_url==url)
+        content,chapter_start_url=await get_chapter_content(page,chapter_start_url)
+        chapter_start_url=url+chapter_start_url
+
+
+
 
 
 if __name__ == '__main__':
     url = 'http://www.skwen.me/13/13577/'
     loop = asyncio.get_event_loop()  #协程，开启个无限循环的程序流程，把一些函数注册到事件循环上。当满足事件发生的时候，调用相应的协程函数。
     loop.run_until_complete(main(url))
+
 
 
 
