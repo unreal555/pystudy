9feee2a95b8e87abe8c6731966df73aaf569772f
unreal555 <47540479@qq.com>
2020-05-30 15:52:56 +0800

    默认的

--- ting89.py
+++ ting89.py
@@ -21,14 +21,18 @@ else:
 
 all=re.findall('''<li><ahref='(.*?)'target="_blank">(.*?)</a></li>''',page)
 
+count=0
 for i in all:
-    print(domain+i[0])
-    chapter=domain+i[0]
-    response=requests.get(chapter)
-    page=response.content.decode('gbk')
-    page=mytools.qu_kong_ge(page)
-    down=re.findall(r'''下载保存时请自行重命名</div><iframesrc="http://play.ting89.com/down/down.php\?url=(.*?)"height''',page)
+    if count<10:
+        print(domain+i[0])
+        chapter=domain+i[0]
+        response=requests.get(chapter)
+        page=response.content.decode('gbk')
+        page=mytools.qu_kong_ge(page)
+        down=re.findall(r'''下载保存时请自行重命名</div><iframesrc="http://play.ting89.com/down/down.php\?url=(.*?)"height''',page)
 
-    response=requests.get(down[0])
-    with open('./{}/{}'.format(bookname,down[0].split('/')[-1]),'wb') as f:
-        f.write(response.content)
+        response=requests.get(down[0])
+        with open('./{}/{}'.format(bookname,down[0].split('/')[-1]),'wb') as f:
+            f.write(response.content)
+    else:
+        break
