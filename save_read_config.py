7a978f4e9ffc79cd775e39bc89a1f24144184ada
Administrator <47540479@qq.com>
2020-06-17 11:36:10 +0800

    默认的

---
+++ save_read_config.py
@@ -0,0 +1,13 @@
+import configparser
+import os
+
+config=configparser.ConfigParser()
+
+config_file_name='config.ini'
+print('111')
+# print(config.read(os.path.join('.','/')))
+#
+# if os.path.exists(os.path.join('.','/')):
+#
+#     config=configparser.ConfigParser
+#     print(configparser)
