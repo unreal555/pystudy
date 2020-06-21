7566cd2c5ef8a80248f0eacf6fd8bb0eb0369bc1
unreal555 <47540479@qq.com>
2020-06-21 20:49:13 +0800

    默认的

--- 书连网-外发版.py
+++ 书连网-外发版.py
@@ -10,10 +10,18 @@ import os
 import random
 from time import sleep
 
-
+'''
+账号13177551358
+密码zxc123456
+爬取小说
+http://vip.shulink.com/files/article/info/68/68033.htm
+http://vip.shulink.com/files/article/info/69/69330.htm
+两个'''
 
 domain = 'http://vip.shulink.com/'
-chapurl = 'http://vip.shulink.com/files/article/html/139/139548/index.html'
+
+# chapurl = 'http://vip.shulink.com/files/article/html/68/68033/index.html'
+chapurl = 'http://vip.shulink.com/html/69/69330/indexasc.html2'
 
 headers = {}
 headers['Accept-Encoding'] = 'gzip, deflate'
@@ -26,11 +34,14 @@ headers['Pragma'] = 'no-cache'
 headers['Referer'] = 'http://vip.shulink.com/files/article/html/139/139522/index.html'
 headers['Upgrade-Insecure-Requests'] = '1'
 headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'
-
+headers[
+    'Cookie'] = '''PHPSESSID=fo8fjmqeaph67sq651rtfdro8h; jieqiUserInfo=jieqiUserId%3D379396%2CjieqiUserUname%3D%B6%AB%C4%DE%2CjieqiUserName%3D%26%23x4E1C%3B%26%23x9713%3B%2CjieqiUserGroup%3D3%2CjieqiUserGroupName%3D%26%23x666E%3B%26%23x901A%3B%26%23x4F1A%3B%26%23x5458%3B%2CjieqiUserVip%3D1%2CjieqiUserHonorId%3D5%2CjieqiUserHonor%3D%26%23x65E0%3B%26%23x53CC%3B%26%23x9690%3B%26%23x58EB%3B%2CjieqiUserToken%3Ddc9d608a620ec41e3ceb6645d3c886cb%2CjieqiCodeLogin%3D0%2CjieqiCodePost%3D0%2CjieqiUserPassword%3Ddd9afc93112397f0cf6b597453bb6b8e%2CjieqiUserAccount%3D%26%23x4E1C%3B%26%23x9713%3B%2CjieqiUserLogin%3D1592737763; jieqiVisitInfo=jieqiUserLogin%3D1592737763%2CjieqiUserId%3D379396; jieqiVisitId=article_articleviews%3D68033%7C69330; jieqiRecentRead=68033.5036865.1.1592739461.379396'''
 
 def get_chapter_list(s):
-    temp = re.findall('<dl class="index"(.*?)</dl>', s, re.S)[0]
-    chapter_list = re.findall('href="(.*?)" title=.*?>(.*?)</a>',temp, re.S)
+    temp=qu_kong_ge(s)
+    temp = re.findall('<dlclass="index"(.*?)</dl>', temp, re.S)[0]
+    chapter_list = re.findall('href="(.*?)"title=.*?>(.*?)</a>',temp, re.S)
+    print(chapter_list)
     return chapter_list
 
 def get_title(s):
@@ -74,9 +85,9 @@ def qu_kong_ge(s):
 
 
 response = requests.get(chapurl, headers=headers).content.decode('gbk')
-
+print(response)
 title=get_title(response)
-filepath='./'+title
+filepath='./'+title+'.txt'
 chapter_list=get_chapter_list(response)
 create_file(filepath)
 
@@ -94,23 +105,32 @@ for chapter_url,chapter_name in chapter_list:
     print(url, chapter_name)
 
     with open(filepath, 'a', encoding='utf-8') as f:
-        f.write('\t' + chapter_name)
+        f.write(chapter_name)
         f.write('\r\n\r\n')
 
     result = requests.get(url, headers=headers).content.decode('gbk')
+    print(result)
 
     result = qu_kong_ge(result)
 
-    result = re.findall('<divid="acontent"class="acontent">(.*?)</div>', result, re.S)
 
-    result = re.findall('&emsp;&emsp;(.*?)<br/><br/>', result[0], re.S)
+
+    result = re.findall('<divid="acontent"class="acontent">(.*?</div>)', result, re.S)[0]
+
+    result=result.replace('</div>','<br/><br/>')
+
+    print(result)
+
+    result = re.findall('[&emsp;]*(.*?)<br/>', result, re.S)
+
+    print(result)
 
     for i in result:
         print(i)
         with open(filepath, 'a', encoding='utf-8') as f:
-            f.write('\t' + i + '\r\n\r\n')
+            f.write('\t' + i + '\r\n')
 
     with open(filepath, 'a', encoding='utf-8') as f:
-        f.write('\r\n\r\n')
+        f.write('\r\n')
         f.flush()
     random_wait()
--- 书连网.py
+++ 书连网.py
@@ -19,8 +19,8 @@ headers['Pragma'] = 'no-cache'
 headers['Referer'] = 'http://vip.shulink.com/files/article/html/139/139522/index.html'
 headers['Upgrade-Insecure-Requests'] = '1'
 headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'
-# headers[
-#     'Cookie'] = '''PHPSESSID=kt6ivbputhckcch8cu239gte4g; jieqiUserInfo=jieqiUserId%3D369080%2CjieqiUserUname%3D%B6%AB%BC%D2%D6%D6%CA%F7%2A369080%2CjieqiUserName%3D%26%23x4E1C%3B%26%23x5BB6%3B%26%23x79CD%3B%26%23x6811%3B%26%23x002A%3B%26%23x0033%3B%26%23x0036%3B%26%23x0039%3B%26%23x0030%3B%26%23x0038%3B%26%23x0030%3B%2CjieqiUserGroup%3D3%2CjieqiUserGroupName%3D%26%23x666E%3B%26%23x901A%3B%26%23x4F1A%3B%26%23x5458%3B%2CjieqiUserVip%3D1%2CjieqiUserHonorId%3D6%2CjieqiUserHonor%3D%26%23x4E16%3B%26%23x5916%3B%26%23x9AD8%3B%26%23x4EBA%3B%2CjieqiUserToken%3D91b313368769da4bdb9979a68b0c765b%2CjieqiCodeLogin%3D0%2CjieqiCodePost%3D0%2CjieqiUserAccount%3D%26%23x4E1C%3B%26%23x5BB6%3B%26%23x79CD%3B%26%23x6811%3B%26%23x002A%3B%26%23x0033%3B%26%23x0036%3B%26%23x0039%3B%26%23x0030%3B%26%23x0038%3B%26%23x0030%3B%2CjieqiUserLogin%3D1590120689; jieqiVisitInfo=jieqiUserLogin%3D1590120689%2CjieqiUserId%3D369080; '''
+headers[
+    'Cookie'] = '''PHPSESSID=kt6ivbputhckcch8cu239gte4g; jieqiUserInfo=jieqiUserId%3D369080%2CjieqiUserUname%3D%B6%AB%BC%D2%D6%D6%CA%F7%2A369080%2CjieqiUserName%3D%26%23x4E1C%3B%26%23x5BB6%3B%26%23x79CD%3B%26%23x6811%3B%26%23x002A%3B%26%23x0033%3B%26%23x0036%3B%26%23x0039%3B%26%23x0030%3B%26%23x0038%3B%26%23x0030%3B%2CjieqiUserGroup%3D3%2CjieqiUserGroupName%3D%26%23x666E%3B%26%23x901A%3B%26%23x4F1A%3B%26%23x5458%3B%2CjieqiUserVip%3D1%2CjieqiUserHonorId%3D6%2CjieqiUserHonor%3D%26%23x4E16%3B%26%23x5916%3B%26%23x9AD8%3B%26%23x4EBA%3B%2CjieqiUserToken%3D91b313368769da4bdb9979a68b0c765b%2CjieqiCodeLogin%3D0%2CjieqiCodePost%3D0%2CjieqiUserAccount%3D%26%23x4E1C%3B%26%23x5BB6%3B%26%23x79CD%3B%26%23x6811%3B%26%23x002A%3B%26%23x0033%3B%26%23x0036%3B%26%23x0039%3B%26%23x0030%3B%26%23x0038%3B%26%23x0030%3B%2CjieqiUserLogin%3D1590120689; jieqiVisitInfo=jieqiUserLogin%3D1590120689%2CjieqiUserId%3D369080; '''
 
 def get_chapter_list(s):
     temp = re.findall('<dl class="index"(.*?)</dl>', s, re.S)[0]
@@ -62,7 +62,7 @@ for chapter_url,chapter_name in chapter_list:
     print(url, chapter_name)
 
     with open(filepath, 'a', encoding='utf-8') as f:
-        f.write('\t' + chapter_name)
+        f.write(chapter_name)
         f.write('\r\n\r\n')
 
     result = requests.get(url, headers=headers).content.decode('gbk')
@@ -76,9 +76,9 @@ for chapter_url,chapter_name in chapter_list:
     for i in result:
         print(i)
         with open(filepath, 'a', encoding='utf-8') as f:
-            f.write('\t' + i + '\r\n\r\n')
+            f.write('\t' + i + '\r\n')
 
     with open(filepath, 'a', encoding='utf-8') as f:
-        f.write('\r\n\r\n')
+        f.write('\r\n')
         f.flush()
     mytools.random_wait()
