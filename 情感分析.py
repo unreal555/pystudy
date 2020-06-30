9b64603c7417d8fe6a8f16c45466158190a3a3b9
unreal555 <47540479@qq.com>
2020-06-30 21:06:46 +0800

    默认的

--- 情感分析.py
+++ 情感分析.py
@@ -1,19 +1,33 @@
 from snownlp import SnowNLP
 import pandas as pd
+import os
 
-db=pd.read_csv(r'C:\Users\Administrator\Desktop\地王.csv',encoding='utf-8')
-print(db)
-dup=db.drop_duplicates(subset=['title'],keep='first')
-dup['senti']=None
+path='d:/dir'
 
 
+def dup(source,des):
+    if not os.path.exists(os.path.join(root,'去重')):
+        os.makedirs(os.path.join(root,'去重'))
+    db=pd.read_csv(source,encoding='utf-8')
+    print(db)
+    dup=db.drop_duplicates(subset=['title'],keep='first')
+    print(dup)
+    dup.to_csv(des,index=False,encoding='gb18030')
 
-for index,row in dup.iterrows():
-    text=row['title']
-    s=SnowNLP(text).sentiments
-    row['senti']=s.sentiments
 
 
+for root, dirnames, filenames in os.walk(path):
+    for filename in filenames:
+        source=os.path.join(root,filename)
+        des=os.path.join(root,'去重',filename)
+        dup(source,des)
 
-print(dup)
-dup.to_csv('./text.csv',index=False,encoding='gb18030')
+
+#
+#
+# for index,row in dup.iterrows():
+#     text=row['title']
+#     s=SnowNLP(text).sentiments
+#     row['senti']=s.sentiments
+
+
