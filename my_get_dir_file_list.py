65d2481926b256606778e86eeca1401408cab24a
Administrator <47540479@qq.com>
2020-08-19 11:05:58 +0800

    默认的

--- my_get_dir_file_list.py
+++ my_get_dir_file_list.py
@@ -6,25 +6,39 @@
 
 import os
 
-def get_dir_files(path,*ext):
+def get_dirs_files_list(path,*ext):
     '''
     :param path: 要分析的路径
-    :param ext:   扩展名,逗号分隔的str
+    :param ext:   dir或者扩展名,逗号分隔的str
     :return:    False 或者文件列表
     '''
+    path=os.path.abspath(path)
     try:
         if os.path.exists(path) and os.path.isdir(path):
             result=[os.path.join(path,x) for x in os.listdir(path)]
 
+
+
             if len(ext)==0:
                 print('无类型筛选,返回所有文件')
-                for i in result:print(i)
+                print(result)
                 return result
+
+            if ext[0]=='''dir''':
+                dirs=[]
+                print('返回目录')
+                for i in result:
+                    if os.path.isdir(i):
+                        dirs.append(i)
+                print(dirs)
+                return  dirs
+
+
             if len(ext)>0:
                 print('返回{}类型的文件'.format(ext))
-
                 result=[x for x in result if os.path.splitext(x)[1].split('.')[-1] in ext]
-                for i in result:print(i)
+                print(result)
+                return result
 
         else:
             return False
@@ -33,4 +47,10 @@ def get_dir_files(path,*ext):
 
               
 if __name__ == '__main__':
-    get_dir_files(r'C:\Users\Administrator\Desktop','txt','xls')
+    for i in get_dirs_files_list(r'G:\20200819','dir'):
+        days={}
+        for x in get_dirs_files_list(i):
+            days[int(x[-8:-1])]=[x,*get_dirs_files_list(x)]
+        print(days)
+
+
