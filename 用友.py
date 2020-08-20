ed2cc5a4c1a6c81f947b9b105bf9694213e7d658
Administrator <47540479@qq.com>
2020-08-20 10:14:58 +0800

    默认的

--- 用友.py
+++ 用友.py
@@ -5,6 +5,8 @@
 # Tool ：PyCharm
 
 import os
+import shutil
+import re
 
 def get_dirs_files_list(path,*ext):
     '''
@@ -20,13 +22,13 @@ def get_dirs_files_list(path,*ext):
 
 
             if len(ext)==0:
-                print('无类型筛选,返回所有文件')
+                print('无类型筛选,返回所有文件目录')
                 print(result)
                 return result
 
             if ext[0]=='''dir''':
                 dirs=[]
-                print('返回目录')
+                print('只返回目录')
                 for i in result:
                     if os.path.isdir(i):
                         dirs.append(i)
@@ -49,11 +51,42 @@ def get_dirs_files_list(path,*ext):
 
 if __name__ == '__main__':
     source=r'''X:\自动备份'''
-    des=os.path.join('''X:\自动备份''','des')
-    for i in get_dirs_files_list(path,'dir'):
+    des=os.path.join(source,'des')
+
+    if not os.path.exists(des):
+        os.makedirs(des)
+
+    for zhangtao in get_dirs_files_list(source,'dir'):
+
+        if zhangtao==des:
+            continue
+
         days={}
-        for x in get_dirs_files_list(i):
+        for x in get_dirs_files_list(zhangtao):
             days[int(x[-8:])]=[x,*get_dirs_files_list(x,'BA_','Lst')]
         print(days)
+
+        if len(days)==0:
+            pass
+
+        if len(days)==1:
+            shutil.move(zhangtao, des)
+
+        if len(days)>1:
+            for key in sorted(days.keys(),reverse=True):
+
+                if len(days[key])==3:
+                    s=days[key][0]
+                    d=os.path.join(des,re.findall(r'ZT[0-9]{3,}',days[key][0])[0])
+
+                    if not os.path.exists(d):
+                        os.makedirs(d)
+
+
+                    shutil.move(s,d)
+                    break
+
+
+
 
 
