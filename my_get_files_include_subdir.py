fd879a5aad11df7b4d2352ad3f9ef771aff3facc
Administrator <47540479@qq.com>
2020-08-28 17:40:17 +0800

    默认的

--- my_get_files_include_subdir.py
+++ my_get_files_include_subdir.py
@@ -8,22 +8,31 @@ import os
 import  re
 
 def get_files(path,*ext,debug=False):
-    all=[]
+    all_files=[]
+    all_dirs=[]
     ext=[str.lower(x) for x in ext]
     for basedir,subdirs,files in os.walk(path,topdown=0):
         if len(files)==0:
             continue
+        all_dirs.append(basedir)
         for file in files:
             if debug:print(r'当前目录为:  {}，文件为:  {}'.format(basedir,file))
-            all.append(os.path.join(basedir, file))
+            all_files.append(os.path.join(basedir, file))
 
     if len(ext)==0:
-        print('返回所有文件')
+        print('无类型筛选,返回所有文件')
         return all
 
-    if len(ext)>0:
+    if len(ext)>0 and 'dir' not in ext:
+        print('返回{}类型的文件'.format(ext))
         result=[x for x in all if str.lower(re.split(r'\.',x)[-1]) in ext]
         return result
 
+    if len(ext)>0 and 'dir' in ext:
+        print('目录和文件,请分开筛选,返回目录,本次只返回目录')
+        return all_dirs
+
+
+
 if __name__ == '__main__':
-    print(get_files(r'e:/','Jpg','zip',debug=True))
+    print(get_files(r'e:/','dir',debug=True))
