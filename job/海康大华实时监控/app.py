2b09a1daf54667d5748532ae0511573cd72f16e2
unreal555 <47540479@qq.com>
2021-01-16 08:57:21 +0800

    clean

--- job/海康大华实时监控/app.py
+++ job/海康大华实时监控/app.py
@@ -1,4 +1,4 @@
-﻿# coding:utf-8
+﻿
 # ﻿ Team : JiaLiDun University
 # Author：zl
 # Date ：2020/9/30 0030 上午 9:39
@@ -28,6 +28,7 @@ REC_PATH = './rec'
 DAT_PATH = './dat'
 DEFAULT_COLOR = '#bcbcbc'
 VIDEO_DEFAULT_COLOR = '#acbcbc'
+FONT_COLOR='black'
 REFRESH_TIME=600
 
 
@@ -40,26 +41,28 @@ class my_app():
 
 	def __init__(self):
 		self.root = Tk()
-
 		set_icon(self.root)
-
 		self.root.title('Player')
-
 		self.root['bd']=0
 		self.root['bg'] = DEFAULT_COLOR
 		self.root.attributes("-alpha", 0.9)
 		#self.root.resizable(0, 0)  # 设置窗口大小不可变
-
-
 		self.root.overrideredirect()
 		max_x,max_y=self.root.maxsize()
-
 		self.root.geometry("%sx%s+%s+%s"%(int(max_x*0.9),int(max_y*0.9),int(max_x*0.05),int(max_y*0.02)))
+
+		self.float_window = tk.Toplevel(self.root)
+		self.float_window.geometry('1x1')
+		self.float_label=tk.Label(self.float_window,text='')
+		self.float_window.attributes("-alpha", 0.3)  # 透明度(0.0~1.0)
+		self.float_window.overrideredirect(True)  # 去除窗口边框
+		self.float_window.attributes("-toolwindow", True)  # 置为工具窗口(没有最大最小按钮)
+		self.float_window.attributes("-topmost", True)
+
 
 		self.window_des = [('单窗口', 1), ('四窗口', 4), ('九窗口', 9)]
 
 		self.v_num = IntVar()
-		self.v_num.set(9)
 
 		self.info = StringVar()
 		self.info.set('初始化中......')
@@ -136,58 +139,32 @@ class my_app():
 		self.video_play_9 = tk.Frame(self.video_play_area_3, cursor='plus', bd=1, relief="sunken",
 		                             class_='window_9', highlightthickness=2, bg=VIDEO_DEFAULT_COLOR)
 
-		self.video_play_1_state=tk.Label(self.video_play_1,cursor='plus', text='',bd=1, relief="sunken", highlightthickness=2, bg=VIDEO_DEFAULT_COLOR)
-		self.video_play_1_state.pack(side=tk.BOTTOM, anchor=tk.S, expand=tk.NO, fill=tk.BOTH)
 
-		self.video_play_2_state=tk.Label(self.video_play_2,cursor='plus', text='',bd=1, relief="sunken", highlightthickness=2, bg=VIDEO_DEFAULT_COLOR)
-		self.video_play_2_state.pack(side=tk.BOTTOM, anchor=tk.S, expand=tk.NO, fill=tk.BOTH)
-
-		self.video_play_3_state=tk.Label(self.video_play_3,cursor='plus', text='',bd=1, relief="sunken", highlightthickness=2, bg=VIDEO_DEFAULT_COLOR)
-		self.video_play_3_state.pack(side=tk.BOTTOM, anchor=tk.S, expand=tk.NO, fill=tk.BOTH)
-
-		self.video_play_4_state=tk.Label(self.video_play_4,cursor='plus', text='',bd=1, relief="sunken", highlightthickness=2, bg=VIDEO_DEFAULT_COLOR)
-		self.video_play_4_state.pack(side=tk.BOTTOM, anchor=tk.S, expand=tk.NO, fill=tk.BOTH)
-
-		self.video_play_5_state=tk.Label(self.video_play_5,cursor='plus', text='',bd=1, relief="sunken", highlightthickness=2, bg=VIDEO_DEFAULT_COLOR)
-		self.video_play_5_state.pack(side=tk.BOTTOM, anchor=tk.S, expand=tk.NO, fill=tk.BOTH)
-
-		self.video_play_6_state=tk.Label(self.video_play_6,cursor='plus', text='',bd=1, relief="sunken", highlightthickness=2, bg=VIDEO_DEFAULT_COLOR)
-		self.video_play_6_state.pack(side=tk.BOTTOM, anchor=tk.S, expand=tk.NO, fill=tk.BOTH)
-
-		self.video_play_7_state=tk.Label(self.video_play_7,cursor='plus', text='',bd=1, relief="sunken", highlightthickness=2, bg=VIDEO_DEFAULT_COLOR)
-		self.video_play_7_state.pack(side=tk.BOTTOM, anchor=tk.S, expand=tk.NO, fill=tk.BOTH)
-
-		self.video_play_8_state=tk.Label(self.video_play_8,cursor='plus', text='',bd=1, relief="sunken", highlightthickness=2, bg=VIDEO_DEFAULT_COLOR)
-		self.video_play_8_state.pack(side=tk.BOTTOM, anchor=tk.S, expand=tk.NO, fill=tk.BOTH)
-
-		self.video_play_9_state=tk.Label(self.video_play_9,cursor='plus', text='',bd=1, relief="sunken", highlightthickness=2, bg=VIDEO_DEFAULT_COLOR)
-		self.video_play_9_state.pack(side=tk.BOTTOM, anchor=tk.S, expand=tk.NO, fill=tk.BOTH)
-
-		self.video_play_1.bind('<Enter>',self.on_mouse_move_in_area)
+		self.video_play_1.bind('<Motion>',self.on_mouse_move_in_area)
 		self.video_play_1.bind('<Leave>', self.on_mouse_move_out_area)
 
-		self.video_play_2.bind('<Enter>',self.on_mouse_move_in_area)
+		self.video_play_2.bind('<Motion>',self.on_mouse_move_in_area)
 		self.video_play_2.bind('<Leave>', self.on_mouse_move_out_area)
 
-		self.video_play_3.bind('<Enter>',self.on_mouse_move_in_area)
+		self.video_play_3.bind('<Motion>',self.on_mouse_move_in_area)
 		self.video_play_3.bind('<Leave>', self.on_mouse_move_out_area)
 
-		self.video_play_4.bind('<Enter>',self.on_mouse_move_in_area)
+		self.video_play_4.bind('<Motion>',self.on_mouse_move_in_area)
 		self.video_play_4.bind('<Leave>', self.on_mouse_move_out_area)
 
-		self.video_play_5.bind('<Enter>',self.on_mouse_move_in_area)
+		self.video_play_5.bind('<Motion>',self.on_mouse_move_in_area)
 		self.video_play_5.bind('<Leave>', self.on_mouse_move_out_area)
 
-		self.video_play_6.bind('<Enter>',self.on_mouse_move_in_area)
+		self.video_play_6.bind('<Motion>',self.on_mouse_move_in_area)
 		self.video_play_6.bind('<Leave>', self.on_mouse_move_out_area)
 
-		self.video_play_7.bind('<Enter>',self.on_mouse_move_in_area)
+		self.video_play_7.bind('<Motion>',self.on_mouse_move_in_area)
 		self.video_play_7.bind('<Leave>', self.on_mouse_move_out_area)
 
-		self.video_play_8.bind('<Enter>',self.on_mouse_move_in_area)
+		self.video_play_8.bind('<Motion>',self.on_mouse_move_in_area)
 		self.video_play_8.bind('<Leave>', self.on_mouse_move_out_area)
 
-		self.video_play_9.bind('<Enter>',self.on_mouse_move_in_area)
+		self.video_play_9.bind('<Motion>',self.on_mouse_move_in_area)
 		self.video_play_9.bind('<Leave>', self.on_mouse_move_out_area)
 
 		self.video_play_1.bind("<ButtonPress-1>", self.set_select_window_info)
@@ -279,8 +256,6 @@ class my_app():
 		self.label_blank = tk.Label(self.video_control_area, text='   ', anchor='e', justify='right')
 		self.label_blank.pack(side=tk.RIGHT, anchor=tk.S, expand=tk.NO, fill=tk.BOTH)
 
-		self.show_window()
-
 		self.root.bind('<KeyPress-Escape>',self.on_click_esc)
 
 		self.now_hwnd = self.video_play_1.winfo_id()
@@ -291,7 +266,9 @@ class my_app():
 
 		self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
 
-		# self.last_init(event='')
+		self.last_init(event='')
+		# self.v_num.set(4)
+		# self.show_window()
 
 	def on_click_esc(self,event):
 
@@ -316,7 +293,6 @@ class my_app():
 			self.hide_button['text'] = 'showing'
 			return
 
-
 		self.list_area.pack_forget()
 		print(self.list_area.winfo_exists())
 
@@ -326,15 +302,37 @@ class my_app():
 	def on_mouse_leave_hidden_button(self,event):
 		self.hide_button['bg'] = 'white'
 
+
+
 	def on_mouse_move_in_area(self,event):
-		if self.window_status[event.widget.winfo_class()]=='0':
-			pass
-		label=self.get_childen_widget(event)
-		label.pack(side=tk.BOTTOM, anchor=tk.S, expand=tk.NO, fill=tk.BOTH)
+		win_desc=event.widget.winfo_class()
+		info=self.window_status[event.widget.winfo_class()]
+		print(info)
+		if info==0:
+			show_info='窗口：%s，No Singal'%win_desc
+			size_x=10*len(show_info)
+			size_y=25
+		else:
+			server_desc,ip,port,user=info[0].split(':')
+			channel=info[2]
+			if server_desc=='dahua':
+				server_desc='大华'
+			if server_desc=='haikang':
+				server_desc='海康'
+			show_info='窗口：%s，正在播放%s@%s:%s channel %s'%(win_desc,server_desc,ip,port,channel)
+			size_x=10*len(show_info)
+			size_y=25
+		self.float_label['text']=show_info
+		self.float_label.pack(side=tk.RIGHT, anchor=tk.S, expand=tk.YES, fill=tk.BOTH)
+		self.float_window.geometry('%sx%s+%s+%s'%(size_x,size_y,event.x_root+5,event.y_root+5))
+
 
 	def on_mouse_move_out_area(self,event):
-		label=self.get_childen_widget(event)
-		label.pack_forget()
+
+		self.float_label['text']=0
+		self.float_label.pack_forget()
+		self.float_window.geometry('1x1')
+
 
 	def on_closing(self):
 		def do():
@@ -458,9 +456,9 @@ class my_app():
 
 				if isinstance(value, int):
 					continue
-				if server in value and 'haikang:' in value[0] and channel in value:
-					showwarning(message='本cam已在 {} 中播放'.format(window_name))
-					return
+				# if server in value and 'haikang:' in value[0] and channel in value:
+				# 	showwarning(message='本cam已在 {} 中播放'.format(window_name))
+				# 	return
 
 			self.play_hk_cam(server_desc, server, channel, self.now_window_name, self.now_hwnd)
 
@@ -473,9 +471,9 @@ class my_app():
 
 				if isinstance(value, int):
 					continue
-				if server in value and 'dahua:' in value[0] and channel in value:
-					showwarning(message='本cam已在 {} 中播放'.format(window_name))
-					return
+				# if server in value and 'dahua:' in value[0] and channel in value:
+				# 	showwarning(message='本cam已在 {} 中播放'.format(window_name))
+				# 	return
 
 			self.play_dh_cam(server_desc, server, channel, self.now_window_name, self.now_hwnd)
 
@@ -561,12 +559,17 @@ class my_app():
 
 	def save_windows_states(self):
 		states = {}
+		v_num=self.v_num.get()
+		if v_num not in [1,4,9,16]:
+			v_num=9
+		states['v_num']=v_num
 		for win_name in self.window_status:
 			if self.window_status[win_name] == 0:
 				continue
 			else:
 				desc, dvr, channel, handle = self.window_status[win_name]
 				states[win_name] = [desc, channel]
+		print(states)
 		with open(os.path.join(DAT_PATH, 'state.dat'), 'wb') as f:
 			pickle.dump(states, f)
 
@@ -598,13 +601,31 @@ class my_app():
 					states = pickle.load(f)
 					print('加载的上次的配置', states)
 				except:
+					self.v_num.set(9)
+					self.show_window()
 					return
 
-		if states == {}:
+		print('load',states)
+
+		if states=={}:
+			self.v_num.set(9)
+			self.show_window()
+			return
+
+
+		v_num=states['v_num']
+		if v_num not in [1,4,9,16]:
+			v_num=9
+		self.v_num.set(v_num)
+		self.show_window()
+
+		if len(states)==1:
 			return
 
 		if askokcancel('提示', '是否载入上次的视频窗口') == True:
-			for window_name in states:
+			for window_name in states.keys():
+				if window_name=='v_num':
+					continue
 				server_desc, channel = states[window_name]
 				print(server_desc, window_name, channel)
 				hwnd = self.get_window_hwnd(window_name)
@@ -615,11 +636,11 @@ class my_app():
 				if server_desc in self.online_dahua_servers.keys():
 					server = self.online_dahua_servers[server_desc]['instance']
 				print(server_desc, server, channel, window_name, channel, hwnd)
-
 				if server == '':
 					continue
 				else:
 					self.play_hk_cam(server_desc, server, channel, window_name, hwnd)
+
 
 	def get_father_widget(self, event):
 
@@ -627,8 +648,7 @@ class my_app():
 
 	def get_childen_widget(self, event):
 		child=event.widget.winfo_children()
-		if len(child)>0:
-			return event.widget.nametowidget(child[0])
+		return [event.widget.nametowidget(x) for x in child]
 
 	def show_nine_play(self):
 
@@ -737,7 +757,6 @@ class my_app():
 	def show_window(self):
 
 		#  print(self.v_num.get(),self.is_single_playing)
-
 		if self.v_num.get() == 9:
 			self.show_nine_play()
 
