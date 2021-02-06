b3afec13665a5e0d42bf7966b8ec571b89935966
unreal <47540479@qq.com>
2021-02-06 18:59:47 +0800

    clean

---
+++ my_cv2.py
@@ -0,0 +1,55 @@
+import cv2
+import time
+
+try:
+    cap = cv2.VideoCapture(0)
+    cap.open(0, cv2.CAP_DSHOW)  #摄像机太老，必须修改打开方式，猜测是directshow的意思
+    # face_model = cv2.CascadeClassifier('./face_haarcascade/haarcascade_frontalface_alt.xml')
+    face_model = cv2.CascadeClassifier('./face_haarcascade/haarcascade_frontalface_alt.xml')
+except:
+    exit()
+n=1
+start=time.time()
+state=0
+while True:
+    ret, frame = cap.read()
+    frame=cv2.flip(frame, 180)
+    frame=cv2.putText(frame,str(n),(10, 30), cv2.FONT_HERSHEY_DUPLEX, 0.8,(255,0,0), 1)
+    # 图片进行灰度处理
+    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+
+    # 人脸检测
+    faces = face_model.detectMultiScale(gray,minSize=(24, 24))
+    # 标记人脸
+    if len(faces)!=state:
+        if len(faces)-state>0:
+            print('+',len(faces))
+        else:
+            print('-',len(faces))
+        state=len(faces)
+
+
+    for (x, y, w, h) in faces:
+        # 1.原始图片；2坐标点；3.矩形宽
+        #高 4.颜色值(RGB)；5.线框
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        # cv2.circle(frame,(int((x+2*w)/2),int((y+h)/2)),int(h/2),(0,255,0),1)
+        # cv2.ellipse(frame, (int((x+w/)/2),int((y+h)/2)), (int((h+y)/2),int((x+w)/2)), 0, 0, 360, (255, 255, 255), 3) # 画椭圆
+    cv2.imshow('frame',frame)
+
+    n=n+1
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
+del (face_model)
+run_time=time.time()-start
+print(run_time)
+print(n/run_time)
+
+
+
+
---
+++ my_winsound.py
@@ -0,0 +1,59 @@
+import time
+import winsound
+
+节奏=500
+#
+一闪一闪亮晶晶='1155665-4433221-5544332-5544332-1155665-4433221-'
+# 警报='3213213131'
+#
+#
+n1=523
+n2=587
+n3=659
+n4=698
+n5=784
+n6=880
+n7=988
+#
+h1=1046
+h2=1175
+h3=1318
+h4=1397
+h5=1568
+h6=1760
+h7=1976
+#
+def play(音符,t=节奏):
+    winsound.Beep(音符,t)
+#
+# for 音符 in 一闪一闪亮晶晶:
+#     if 音符=='1':
+#         play(h1)
+#     if 音符=='2':
+#         play(h2)
+#     if 音符== '3':
+#         play(h3)
+#     if 音符== '4':
+#         play(h4)
+#     if 音符== '5':
+#         play(h5)
+#     if 音符== '6':
+#         play(h6)
+#     if 音符== '7':
+#         play(h7)
+#     if 音符=='-':
+#         time.sleep(节奏/1000)
+
+
+
+# play(h1)
+
+print(1)
+print(2)
+print('hello world')
+
+n=9
+print(n)
+while n>0:
+    print(n)
+    n=n-1
