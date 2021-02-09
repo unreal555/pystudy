cf8d60492e492c044d4aeb3e280123abe536de49
Administrator <47540479@qq.com>
2021-02-09 16:30:25 +0800

    clean

--- study_PyQt5/test2.py
+++ study_PyQt5/test2.py
@@ -5,39 +5,59 @@
 # Tool ：PyCharm
 
 import sys
-from PyQt5.QtWidgets import QWidget,QApplication,QMainWindow,QPushButton,QVBoxLayout,QGridLayout
+from PyQt5.QtWidgets import QWidget,QApplication,QMainWindow,QPushButton,QVBoxLayout,QGridLayout,QLabel
 from PyQt5 import QtCore
+from PyQt5.QtGui import QPalette,QPixmap
+from PyQt5.QtCore import Qt
 
-def on_click(window,app):
+class my(QWidget):
+	def __init__(self):
+		super(my, self).__init__()
+		self.init(self)
 
-    print(window.sender().text())
-
-    app.quit()
-
-app=QApplication(sys.argv)
-
-window=QWidget()
-
-window.setGeometry(0,0,400,300)
-
-button=QPushButton('退出')
-bbutton=QPushButton('退出')
-
-layout=QGridLayout()
-
-layout.setGeometry(QtCore.QRect(0, 0, 400, 300))
-
-layout.addWidget(button,1,2)
-layout.addWidget(bbutton,4,3)
-
-window.setLayout(layout)
-
-button.clicked.connect(lambda :on_click(window,app))
-
-button.setToolTip('测试')
+	def init(self,argv):
+		label1=QLabel()
+		label1.setText("<font color=yellow>测试</font>")
+		label1.setAutoFillBackground(True)
+		palette=QPalette()
+		palette.setColor(QPalette.Window,Qt.blue)
+		label1.setPalette(palette)
+		label1.setAlignment(Qt.AlignCenter)
 
 
+		label2=QLabel()
+		label2.setText("<a href=http://www.sohu.com>搜狐</a>")
+		label2.setAutoFillBackground(True)
+		label2.setOpenExternalLinks(True)
+		palette.setColor(QPalette.Window,Qt.red)
+		label2.setPalette(palette)
 
-window.show()
-app.exec_()
-              
+		label3=QLabel()
+		label3.setAlignment(Qt.AlignCenter)
+		label3.setToolTip('pic')
+		label3.setPixmap(QPixmap('pic.png'))
+
+		label1.linkHovered.connect(self.mouse_on)
+		label2.linkHovered.connect(self.mouse_on)
+		label3.linkHovered.connect(self.mouse_on)
+
+		vbox=QVBoxLayout()
+
+		vbox.addWidget(label1)
+
+		vbox.addWidget(label2)
+		vbox.addWidget(label3)
+
+		self.setLayout(vbox)
+		self.setWindowTitle('text')
+
+
+
+	def mouse_on(self):
+		print(111)
+
+if __name__ == '__main__':
+	app=QApplication(sys.argv)
+	window=my()
+	window.show()
+	app.exec_()
