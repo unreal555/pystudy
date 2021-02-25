6655f7ccc13c65f2c9207491ebc2e7a0a9eb2d19
Administrator <47540479@qq.com>
2021-02-25 16:24:13 +0800

    clean

--- study_PyQt5/test2.py
+++ study_PyQt5/test2.py
@@ -4,6 +4,30 @@
 # Date ：2021/2/8 0008 上午 9:46
 # Tool ：PyCharm
 
+'''
+用掩码限制QLineEdit控件的输入
+
+A    ASCII字母字符是必须输入的(A-Z、a-z)
+a    ASCII字母字符是允许输入的,但不是必需的(A-Z、a-z)
+N    ASCII字母字符是必须输入的(A-Z、a-z、0-9)
+n    ASII字母字符是允许输入的,但不是必需的(A-Z、a-z、0-9)
+X    任何字符都是必须输入的
+x    任何字符都是允许输入的,但不是必需的
+9    ASCII数字字符是必须输入的(0-9)
+0    ASCII数字字符是允许输入的,但不是必需的(0-9)
+D    ASCII数字字符是必须输入的(1-9)
+d    ASCII数字字符是允许输入的,但不是必需的(1-9)
+#    ASCI数字字符或加减符号是允许输入的,但不是必需的
+H    十六进制格式字符是必须输入的(A-F、a-f、0-9)
+h    十六进制格式字符是允许输入的,但不是必需的(A-F、a-f、0-9)
+B    二进制格式字符是必须输入的(0,1)
+b    二进制格式字符是允许输入的,但不是必需的(0,1)
+>    所有的字母字符都大写
+<    所有的字母字符都小写
+!    关闭大小写转换
+\    使用"\"转义上面列出的字符
+'''
+
 import sys
 from PyQt5.QtWidgets import QWidget,QApplication,QMainWindow,QPushButton,QVBoxLayout,QGridLayout,QLabel,QLineEdit
 from PyQt5 import QtCore
@@ -44,12 +68,13 @@ class my(QWidget):
 
 		input1=QLineEdit()
 		input1.setPlaceholderText('text')
-		input1.setEchoMode(QLineEdit.PasswordEchoOnEdit)
+		input1.setEchoMode(QLineEdit.Normal)
+		input1.setInputMask('HH:HH:HH:HH:HH:HH;-')
 
-		reg=QRegExp('[-]{0,1}[a-zA-Z0-9]{1,3}')
-		validator = QRegExpValidator(reg)
-
-		input1.setValidator(validator)
+		# reg=QRegExp('[-]{0,1}[a-zA-Z0-9]{1,3}')
+		# validator = QRegExpValidator(reg)
+		#
+		# input1.setValidator(validator)
 
 		vbox=QVBoxLayout()
 		vbox.addWidget(label1)
