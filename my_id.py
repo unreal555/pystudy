877d555b59be2675663f29a03e2dda3ade566c83
Administrator <47540479@qq.com>
2021-04-01 15:28:33 +0800

    clean

---
+++ my_id.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+# Team : JiaLiDun University
+# Author：zl
+# Date ：2021/4/1 0001 下午 3:24
+# Tool ：PyCharm
+
+import logging
+
+def calc(s):
+    pass
+              
+def id15to18(s):
+    pass
+
+def check_id(s)
