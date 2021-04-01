e680b0ca4c95333156b2f1c00aa3d7932acfd512
Administrator <47540479@qq.com>
2021-04-01 16:17:59 +0800

    clean

--- my_id.py
+++ my_id.py
@@ -4,13 +4,37 @@
 # Date ：2021/4/1 0001 下午 3:24
 # Tool ：PyCharm
 
-import logging
+import re
 
-def calc(s):
-    pass
-              
+def calc(i):
+    print(i)
+    s=[7,9,10,5,8,4,2,1,6,3,7,9,10,5,8,4,2]   #系数
+    n2k=['1','0','X','9','8','7','6','5','4','3','2']  #余数转换
+    if re.fullmatch('[0-9]{17}',i):
+        i=[int(x) for x in list(i)]
+    else:
+        print('输入有误')
+        return False
+    n=sum(map(lambda x,y:x*y,i,s))%11
+    last_n=n2k[n]
+    return last_n
+
 def id15to18(s):
     pass
 
-def check_id(s):
-    pass
+def check_id(id):
+    reg18='''^[1-9]\d{5}(18|19|2\d)\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$'''
+
+    if not re.fullmatch(reg18,id):
+        print('身份证号码有误')
+        return False
+    i=id[0:17]
+    key=id[17]
+    if key==calc(i):
+        print('match')
+    else:
+        print('校验码有误')
+
+
+#calc('37010220080806010')
+check_id('370102197901210050')
