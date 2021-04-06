f8ae3501d6f4e3f790921ec2d2a234ba3f73887e
unreal555 <47540479@qq.com>
2021-04-06 23:00:18 +0800

    clean

--- job/邮件改附件 msg/set_xls.py
+++ job/邮件改附件 msg/set_xls.py
@@ -12,16 +12,15 @@ import win32com.client as wc  # wc这个别名好像不太好, 嘿嘿
 from win32com.client import constants
 import os
 
-# file=os.path.abspath('./demo.xls')
-# excel = wc.Dispatch("Excel.Application")
+file=os.path.abspath('./demo.xls')
+excel = wc.Dispatch("Excel.Application")
 # excel.Visible=1
-# x=excel.Workbooks.Open(file)
-# print(x.author)
-# x.Author = "Jean Selva"
-# print(x.Author)
-# print(x)
-# x.Save()
+x=excel.Workbooks.Open(file)
+print(x.author)
+sht = x.Worksheets('Sheet1')
+x.Author = "Jean Selva"
+print(x.BuiltinDocumentProperties("Last author").value)
+print(x.BuiltinDocumentProperties("Last save time").value)
 
-a=xlrd.open_workbook('./demo.xls')
+x.Save()
 
-print(xlwt.Workbook.save(a))
