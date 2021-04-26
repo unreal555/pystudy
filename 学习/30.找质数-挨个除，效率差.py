f4f35b600d21ee63a451fbb6e2fa849787d310b6
Administrator <47540479@qq.com>
2021-04-26 14:56:44 +0800

    clean

--- 学习/30.找质数-挨个除，效率差.py
+++ 学习/30.找质数-挨个除，效率差.py
@@ -4,6 +4,7 @@ import time
 
 class prime():
     ##生成器，用于生成数字序列
+    values=[]
     def gen(self,x=2,lim=1000):
         x=int(x)
         if x>lim:
@@ -14,34 +15,30 @@ class prime():
 
     def find(self):
         for num in self.N:
+            #print(num)
             if num<=1 or not isinstance(num,int):
                 # print(num,"worng")
                 continue
             if num==2 or num==3:
-                self.result.append(num)
+                self.values.append(num)
                 continue
             i=2
             while i<=(num//2):
                 if num%i==0:
-                    # print("{}可以整除{}".format(num,i),"所以",num,"不是质数")
-                    i=i+1
+                    #print("{}可以整除{}".format(num,i),"所以",num,"不是质数")
                     break
-                elif i==num//2:
-                    # print(num,"是质数")
-                    self.result.append(num)
+                if i==num//2:
+                    #print(num,"是质数")
+                    self.values.append(num)
                     break
-                else:
-                    # print("{}不可以整除{}".format(num, i), "继续测试")
-                    i=i+1
-        print(self.result,self.result.__len__())
+                i = i + 1
 
     def __init__(self,x=2,lim=100):
-        self.result=[]
         self.N=self.gen(x,lim)
-        print('init',self.result,self.N)
         self.find()
 
 
 start=time.time()
 prime(1,100000)
+print('个数:',len(prime.values),prime.values)
 print(time.time()-start)
