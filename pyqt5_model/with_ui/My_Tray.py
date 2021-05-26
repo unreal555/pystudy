60a24e49f8a0cf6d7ddf2f56289b3778237a34b1
Administrator <47540479@qq.com>
2021-05-26 15:37:37 +0800

    clean

--- pyqt5_model/with_ui/My_Tray.py
+++ pyqt5_model/with_ui/My_Tray.py
@@ -8,12 +8,19 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
+from ui import Ui_Form
 
 class TrayIcon(QSystemTrayIcon):
     def __init__(self, parent=None):
         super(TrayIcon, self).__init__(parent)
+        self.activated.connect(self.iconClicked)
+        #把鼠标点击图标的信号和槽连接
+        self.messageClicked.connect(self.mClicked)
+        #把鼠标点击弹出消息的信号和槽连接
+        self.setIcon(QIcon("ico.ico"))
+        self.icon = self.MessageIcon()
+        #设置图标
         self.showMenu()
-        self.other()
 
     def showMenu(self):
         "设计托盘的菜单，这里我实现了一个二级菜单"
@@ -33,53 +40,47 @@ class TrayIcon(QSystemTrayIcon):
         self.menu1.setTitle("二级菜单")
         self.setContextMenu(self.menu)
 
-    def other(self):
-        self.activated.connect(self.iconClied)
-        #把鼠标点击图标的信号和槽连接
-        self.messageClicked.connect(self.mClied)
-        #把鼠标点击弹出消息的信号和槽连接
-        self.setIcon(QIcon("ico.ico"))
-        self.icon = self.MessageIcon()
-        #设置图标
 
-    def iconClied(self, reason):
+    def iconClicked(self, reason):
         "鼠标点击icon传递的信号会带有一个整形的值，1是表示单击右键，2是双击，3是单击左键，4是用鼠标中键点击"
         if reason == 2 or reason == 3:
-
-            pw = self.parent()
             self.setVisible(False)
-            pw.setVisible(True)
-        print(reason)
+            self.parent().setVisible(True)
+            self.parent().showNormal()
 
-    def mClied(self):
+    def mClicked(self):
         self.showMessage("提示", "你点了消息", self.icon)
 
     def showM(self):
-
         self.showMessage("测试", "我是消息", self.icon)
 
     def quit(self):
         "保险起见，为了完整的退出"
         self.setVisible(False)
-        self.parent().exit()
-        qApp.quit()
-        sys.exit()
+        self.destroy()
+        self.parent().destroy()
 
-class window(QWidget):
+
+class window(QWidget,Ui_Form):
     def __init__(self, parent=None):
         super(window, self).__init__(parent)
+        self.setupUi(self)
         self.setWindowIcon(QIcon('ico.ico'))
+        self.show()
         self.tray = TrayIcon(self)
 
     def hideEvent(self, QHideEvent):
         self.setVisible(False)
         self.tray.setVisible(True)
-
-
+        
+    def closeEvent(self, a0: QCloseEvent) -> None:
+        self.destroy()
+        self.tray.destroy()
+        
 if __name__ == "__main__":
     import sys
     app = QApplication(sys.argv)
     main = window()
-    main.show()
+
 
     sys.exit(app.exec_())
