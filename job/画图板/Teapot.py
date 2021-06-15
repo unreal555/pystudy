4d3627a7511540709390db91eaa9bc3d133861ec
Administrator <47540479@qq.com>
2021-06-15 16:28:19 +0800

    clean

---
+++ job/画图板/Teapot.py
@@ -0,0 +1,89 @@
+# 导入相关库
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+
+
+# 初始化OpenGL函数
+glutInit()
+
+def init():
+    # 材质反光性设置
+    mat_specular = [1.0, 1.0, 1.0, 1.0]  # 镜面反射参数
+    mat_shininess = [50.0]  # 高光指数
+    light_position = [-400.0, -300.0, 200.0, 100.0]  # 光源位置
+    white_light = [255.0, 1.0, 1.0, 1.0]  # 灯位置(1,1,1), 最后1-开关
+    light_model_ambient = [0.2, 0.2, 0.2, 1.0]  # 环境光参数
+
+    glClearColor(0.0, 0.0, 0.0, 0.0)  # 背景色
+    glShadeModel(GL_SMOOTH)  # 填充模式
+
+    # 材质属性 material property
+    glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular)
+    glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess)
+
+    # 灯光设置
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position)
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, white_light)  # 散射光属性
+    glLightfv(GL_LIGHT0, GL_SPECULAR, white_light)  # 镜面反射光
+    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, light_model_ambient)
+
+    glEnable(GL_LIGHTING)  # 开关:使用光
+    glEnable(GL_LIGHT0)  # 打开0#灯
+    # glEnable(GL_DEPTH_TEST)  # 打开深度测试
+    
+    glutDisplayFunc(display)
+    glutReshapeFunc(reshape)
+    glutIdleFunc(display)
+    glutCloseFunc(close)
+    glutMouseFunc(mouseclick)
+
+def mouseclick(button,state,x,y):
+    print(button,state,x,y)
+    if button==2:
+        glLightfv(GL_LIGHT0, GL_POSITION, [x-800, y-600, 0.0, 1.0])
+        
+    
+
+def display():
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glutWireTeapot(0.5)
+    # glutSolidTeapot(0.5)  # 茶壶绘制
+    glRotatef(0.1, 0, 5, 0)  # (角度,x,y,z)
+    glFlush()
+
+
+def reshape(w, h):
+    glViewport(0, 0, w, h)
+    # 设置投影参数
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    # 正交投影
+    if w <= h:
+        glOrtho(-1.5, 1.5, -1.5 * h / w, 1.5 * h / w, -10.0, 10.0)
+    else:
+        glOrtho(-1.5 * w / h, 1.5 * w / h, -1.5, 1.5, -10.0, 10.0)
+    # 设置模型参数--几何体参数
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+
+def close():
+    glutLeaveMainLoop()
+    glutDestroyWindow(glutGetWindow())
+    import main6
+
+
+def start():
+
+    glutInitDisplayMode(GLUT_SINGLE| GLUT_RGB | GLUT_DEPTH)
+    glutInitWindowSize(800, 600)
+    glutInitWindowPosition(100, 100)
+    glutCreateWindow("3D")
+    init()
+    glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_GLUTMAINLOOP_RETURNS)
+    glutMainLoop()
+
+
+if __name__ == "__main__":
+
+    start()
